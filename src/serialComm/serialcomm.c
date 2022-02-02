f4cf3400f29da5efcb57e121b3b73c5cafe27e12
ikimon <4580tatsuya@gmail.com>
2022-02-02 22:38:11 +0900

    シリアル通信プロジェクトを追加

--- src/serialComm/serialcomm.c
+++ src/serialComm/serialcomm.c
@@ -4,18 +4,38 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+#include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/uart.h"
 
-#define BACK_LIGHT_PIN 
+#define UART_ID uart0
+#define BAUD_RATE 115200
 
-int main() {
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-    while (true) {
-        gpio_put(LED_PIN, 1);
-        sleep_ms(250);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(250);
-    }
+// We are using pins 0 and 1, but see the GPIO function select table in the
+// datasheet for information on which other pins can be used.
+#define UART_TX_PIN 0
+#define UART_RX_PIN 1
+
+int main()
+{
+
+    // Set up our UART with the required speed.
+    uart_init(UART_ID, BAUD_RATE);
+
+    // Set the TX and RX pins by using the function select on the GPIO
+    // Set datasheet for more information on function select
+    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+
+    // Use some the various UART functions to send out data
+    // In a default system, printf will also output via the default UART
+
+    // Send out a character without any conversions
+    uart_putc_raw(UART_ID, 'A');
+
+    // Send out a character but do CR/LF conversions
+    uart_putc(UART_ID, 'B');
+
+    // Send out a string, with CR/LF conversions
+    uart_puts(UART_ID, " Hello, UART!\n");
 }
